328b43332bb6b0d0b486225b7a71b3c0887f8580
Luka Lortkipanidze <89572091+l0rtk@users.noreply.github.com>
2022-06-13 21:24:18 +0400

    removed key

--- run.py
+++ run.py
@@ -14,11 +14,3 @@ client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster0.ekdn
 db = client.test
 print(db)
 
-# client = tweepy.Client('AAAAAAAAAAAAAAAAAAAAAM06SgEAAAAAkZPNYb1U5pH63EdCMuFl%2BhRahYY%3DmWFcsHOU3r1xWbpdIbdyrmI31SuFuifzWt7w0Xgm1E5zSdf9k2')
-
-
-# req = client.search_recent_tweets(query='messi',max_results=10)
-
-# pprint(req)
-# # for tweet in tweets:
-#     # print(tweet)
